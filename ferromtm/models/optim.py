c90bfab65e9a56ee8e620ae9a90f5bcf5618388e
Benjamin Vial <b.vial@qmul.ac.uk>
2019-04-11 19:36:05 +0100

    topopt homogenization

--- ferromtm/models/optim.py
+++ ferromtm/models/optim.py
@@ -6,14 +6,30 @@ from pytheas.optim import TopOpt
 import tempfile
 from aotomat.tools.plottools import *
 
-eps_interp = [1, 13]
+eps_interp = [1, 21]
+
+
+def init_pattern():
+    # # material pattern
+    nmatx, nmaty = 2 ** 8, 2 ** 8
+    mat = genmat.MaterialDensity()
+    mat.xsym = True
+    mat.n_x, mat.n_y, mat.n_z = nmatx, nmaty, 1
+    mat.p_seed = mat.mat_rand
+    mat.nb_threshold = 2
+    # mat.sym8 = True
+    sigma_pat = 20
+    mat.ratio_filter = [sigma_pat, sigma_pat, 1]
+    return mat
+
 
 mat = init_pattern()
+
 mat.pattern = mat.normalized_pattern
-parmesh = 14
+parmesh = 11
 fem_es = init_es(0, 0, incl=False, mat=mat, parmesh=parmesh, mesh_refine=False)
 fem_hom = init_hom(fem_es)
-
+fem_hom.pola = "TM"
 
 # ##########################################
 # #########  OPTIMIZATION PARAMETERS  ######
@@ -33,7 +49,7 @@ to.Nitmax = 8  # maximum number of global iterations
 to.N0 = 0  # initial global iterations
 # to.beta = 1  # projection parameter
 lmin = 1
-to.rfilt = 0.05 * lmin  # filter radius
+to.rfilt = 0.00005 * lmin  # filter radius
 to.filt_weight = "gaussian"
 to.dg_dp = 0
 to.eps_interp = eps_interp
@@ -85,11 +101,18 @@ def f_obj(
     fem_es=fem_es,
     fem_hom=fem_hom,
 ):
+    sens_ana = np.size(grad) > 0
     fem_hom.adjoint = sens_ana
+    # print(sens_ana)
     epsilon = to.make_epsilon(p, filt=filt, proj=proj)
     epsi = epsilon, epsilon, epsilon
     eps_hom_xx, fem_hom = compute_hom_pb_y(fem_hom, epsi, verbose=verbose)
+    print("eps_hom_xx = ", eps_hom_xx)
+    # print("1/eps_hom_xx = ", 1/eps_hom_xx)
+
+    print("objtest = ", np.abs(1 / eps_hom_xx - 1 / 3) ** 2 * 3 ** 2)
     obj = to.get_objective()
+    obj = np.abs(1 / 3 - obj) ** 2 * 3 ** 2
     # print("objective: ", obj)
     if sens_ana:
         sens = to.get_sensitivity(p, filt=filt, proj=proj)
@@ -97,6 +120,29 @@ def f_obj(
         sens = 0
     # fem_hom.postpro_fields(filetype="pos")
     # fem_hom.open_gmsh_gui()
+
+    # plt.clf()
+    # print(sens)
+    # sensplt = to.mesh2grid(sens)
+    # plt.imshow(sensplt)
+    # plt.colorbar()
+
+    plt.clf()
+    adj = to.get_adjoint()
+    print(adj)
+    adjplt = to.mesh2grid(adj.real)
+    plt.imshow(adjplt)
+    plt.colorbar()
+    plt.pause(1)
+
+    # plt.clf()
+    # deq_deps = to.get_deq_deps()
+    # print(deq_deps)
+    # deq_deps_plt = to.mesh2grid(deq_deps.real)
+    # plt.imshow(deq_deps_plt)
+    # plt.colorbar()
+    # cds
+
     if rmtmpdir:
         fem_hom.rm_tmp_dir()
         fem_es.rm_tmp_dir()
@@ -131,7 +177,7 @@ def main_opt(p0):
             popt = to.make_xsym(popt)
         popt_filt, _ = to.filter_param(popt, grad=False)
         popt = to.get_threshold_design(popt_filt)
-        opt_f = f(popt, np.array([]), filt=False, proj=False)
+        opt_f = f_obj(popt, np.array([]), filt=False, proj=False)
         print("optimum at ", popt)
         print("with value  = ", opt_f)
 
@@ -156,6 +202,7 @@ def make_plots(to, p, filt=True, proj=True):
 if __name__ == "__main__":
     # define initial density p0
     p0 = to.random_pattern(mat)
+    # p0 = 0.5*np.ones_like(p0)
     Ebias = 0
     # c = f_obj(p0, f_obj,coupling=True, rmtmpdir=False)
     out = main_opt(p0)
